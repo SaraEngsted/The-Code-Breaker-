0d3abca74c7a3df9aad8b5e68414b54c35f59b4d
SaraEngsted <sara.andreasen@live.dk>
2021-09-30 14:50:38 +0200

    Hello

--- src/TheCodeBreaker.java
+++ src/TheCodeBreaker.java
@@ -1,5 +1,43 @@
+import java.util.Scanner;
+
 public class TheCodeBreaker {
+
+    //Encoder methode
+    public static String numberCypherEncoder(String Word) {
+
+        String alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+        String encodedString = "";
+
+        for (int i = 0; i < Word.length(); i++) {
+            char stringToIndex = Word.charAt(i);
+
+            int charToIndex = alphabet.indexOf(stringToIndex + 1);
+
+            encodedString = encodedString + charToIndex + ";";
+        }
+
+        return encodedString;
+    }
+
+    public static String numberCypherDecoder(int)
+
+
     public static void main(String[] args) {
+
+        //Encoder
+        String BrammingEncoded = numberCypherEncoder("bramming");
+        System.out.println(BrammingEncoded);
+
+        String ElevatorEncoded = numberCypherEncoder("elevator");
+        System.out.println(ElevatorEncoded);
+
+
+        //Decode
+
+
 
     }
 }
+
+
